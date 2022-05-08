118a51f289c2966f54b3b135672b4d29166219db
Junyan Ren <ren.ju@northeastern.edu>
2022-05-08 10:24:40 -0400

    update customer-news detail

--- newssystem/src/router/IndexRouter.js
+++ newssystem/src/router/IndexRouter.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {HashRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Login from '../views/Login/Login'
+import Detail from '../views/news/Detail'
+import News from '../views/news/News'
 import NewsSandBox from '../views/sandbox/NewsSandBox'
 
 export default function IndexRouter() {
@@ -8,6 +10,8 @@ export default function IndexRouter() {
     <HashRouter>
         <Switch>
             <Route path='/login' component={Login}/>
+            <Route path='/news' component={News}/>
+            <Route path='/detail/:id' component={Detail}/>
             {/* 路由拦截 */}
             <Route path='/' render={()=>
                 localStorage.getItem("token")?
